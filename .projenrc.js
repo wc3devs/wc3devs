ca05cd6b597538a91be8825a75b50ed77f3b0933
Maksim Yersh <edelwud@icloud.com>
2023-01-20 20:05:54 +0300

    chore: project created with projen

---
+++ .projenrc.js
@@ -0,0 +1,37 @@
+const { web } = require("projen");
+const project = new web.NextJsTypeScriptProject({
+  name: "wc3devs",
+  description: "wc3devs page",
+  repository: "https://github.com/wc3devs/wc3devs.git",
+  keywords: ["wc3", "warcraft 3", "wc3devs"],
+  
+  release: true,
+  defaultReleaseBranch: "main",
+  pullRequestTemplateContents: [
+    "---",
+    "By submitting this pull request, I confirm that my contribution is made under the terms of the Apache 2.0 license.",
+  ],
+  
+  minNodeVersion: "16.0.0",
+  workflowNodeVersion: "16.18.1",
+  
+  devDeps: ["@next/eslint-plugin-next", "prettier-plugin-tailwindcss"],
+  
+  docgen: true,
+  codeCov: true,
+  tailwind: true,
+  eslint: true,
+  prettier: true,
+  prettierOptions: {
+    settings: {
+      plugins: ["prettier-plugin-tailwindcss"],
+    },
+  },
+  
+  autoMerge: true,
+  autoApproveUpgrades: true,
+  autoApproveOptions: {
+    allowedUsernames: ["edelwud"],
+  },
+});
+project.synth();
