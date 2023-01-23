d4cf5bed4920e848aa78238d582c4cd4d9d47313
Maksim Yersh <edelwud@icloud.com>
2023-01-23 12:02:29 +0300

    feat(app): use `npm` as a primary package manager

--- .projenrc.js
+++ .projenrc.js
@@ -1,22 +1,25 @@
 const { web } = require("projen");
+const { NodePackageManager } = require("projen/lib/javascript");
+
 const project = new web.NextJsTypeScriptProject({
   name: "wc3devs",
   description: "wc3devs page",
   repository: "https://github.com/wc3devs/wc3devs.git",
   keywords: ["wc3", "warcraft 3", "wc3devs"],
-  
+
   release: true,
   defaultReleaseBranch: "main",
   pullRequestTemplateContents: [
     "---",
     "By submitting this pull request, I confirm that my contribution is made under the terms of the Apache 2.0 license.",
   ],
-  
+
   minNodeVersion: "16.0.0",
   workflowNodeVersion: "16.18.1",
-  
+
+  packageManager: NodePackageManager.NPM,
   devDeps: ["@next/eslint-plugin-next", "prettier-plugin-tailwindcss"],
-  
+
   docgen: true,
   codeCov: true,
   tailwind: true,
@@ -28,11 +31,14 @@ const project = new web.NextJsTypeScriptProject({
       plugins: ["prettier-plugin-tailwindcss"],
     },
   },
-  
+
   autoMerge: true,
   autoApproveUpgrades: true,
   autoApproveOptions: {
     allowedUsernames: ["edelwud"],
   },
 });
+
+project.eslint?.addExtends("plugin:@next/next/recommended");
+
 project.synth();
